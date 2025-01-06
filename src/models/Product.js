65ca894755d0d14e60a738c0f731b0051cd59efa
MrEBONG <thierryngabe@icloud.com>
2025-01-06 10:06:33 +0100

    shy

--- src/models/Product.js
+++ src/models/Product.js
@@ -6,6 +6,11 @@ const productSchema = new mongoose.Schema({
   price: { type: Number, required: true, min: 0 },
   stockQuantity: { type: Number, required: true, min: 0 },
   image: { type: String, required: true },
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true 
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
