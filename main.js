875226e858e0256b2e6880b56becc0ce2ec29ff9
Ebong Thierry <ebongthierry569@gmail.com>
2025-05-09 14:00:50 +0100

    perfect

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://apparellglow.vercel.app'],
+  origin: ['http://localhost:3000', 'https://apparellglow.vercel.app', 'https://apparellglow.store'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
